869c7c89882cbe437374cf8d7c3bc7c00de40e47
Gergely Brautigam <skarlso777@gmail.com>
2016-06-11 17:12:50 +0200

    Initial web page.

--- quest.go
+++ quest.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+    "net/http"
 
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+    router.Static("/css", "./static/css")
+    router.Static("/img", "./static/img")
+    router.LoadHTMLGlob("templates/*")
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "title": "GoQuest",
+        })
+    })
+    router.Run(":9090")
 }
