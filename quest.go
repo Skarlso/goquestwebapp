d2918133f28da0966f65191f1d302aefa3ad0f8e
Gergely Brautigam <skarlso777@gmail.com>
2016-06-21 08:21:16 +0200

    Fixed the State to be a random generated, saved value.

--- quest.go
+++ quest.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+    "crypto/rand"
+    "encoding/base64"
     "encoding/json"
     "io/ioutil"
+    "fmt"
     "log"
     "os"
     "net/http"
 
+    "github.com/gin-gonic/contrib/sessions"
     "github.com/gin-gonic/gin"
     "golang.org/x/oauth2"
     "golang.org/x/oauth2/google"
@@ -33,6 +37,14 @@ type User struct {
 
 var cred Credentials
 var conf *oauth2.Config
+var state string
+var store = sessions.NewCookieStore([]byte("secret"))
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
 
 func init() {
     file, err := ioutil.ReadFile("./creds.json")
@@ -63,22 +75,30 @@ func battleHandler(c *gin.Context) {
     })
 }
 
-
-func getLoginURL() string {
-    return conf.AuthCodeURL("")
+func getLoginURL(state string) string {
+    return conf.AuthCodeURL(state)
 }
 
 func authHandler(c *gin.Context) {
     // Handle the exchange code to initiate a transport.
+    session := sessions.Default(c)
+    retrievedState := session.Get("state")
+    if retrievedState != c.Query("state") {
+        c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+        return
+    }
+
 	tok, err := conf.Exchange(oauth2.NoContext, c.Query("code"))
 	if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
+        return
 	}
 
 	client := conf.Client(oauth2.NoContext, tok)
 	email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
     if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
+        return
 	}
     defer email.Body.Close()
     data, _ := ioutil.ReadAll(email.Body)
@@ -87,11 +107,16 @@ func authHandler(c *gin.Context) {
 }
 
 func loginHandler(c *gin.Context) {
-    c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL() + "'><button>Login with Google!</button> </a> </body></html>"))
+    state = randToken()
+    session := sessions.Default(c)
+    session.Set("state", state)
+    session.Save()
+    c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
 }
 
 func main() {
     router := gin.Default()
+    router.Use(sessions.Sessions("goquestsession", store))
     router.Static("/css", "./static/css")
     router.Static("/img", "./static/img")
     router.LoadHTMLGlob("templates/*")
